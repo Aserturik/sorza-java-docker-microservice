b6ac507290056703c7605003b6ba541f0a62b779
Aserturik <alex.hernandez27@uptc.edu.co>
2025-05-06 15:43:18 -0500

    sorza 4

--- cqrs-es/cqrs/src/main/java/com/banking/cqrs/core/infrastucture/CommandDispatcher.java
+++ cqrs-es/cqrs/src/main/java/com/banking/cqrs/core/infrastucture/CommandDispatcher.java
@@ -1,9 +1,9 @@
 package com.banking.cqrs.core.infrastructure;
 
 import com.banking.cqrs.core.commands.BaseCommand;
-import com.banking.cqrs.core.commands.CommandHandlerMethod;
+import com.banking.cqrs.core.commands.CommandHandleMethod;
 
 public interface CommandDispatcher {
-    <T extends BaseCommand> void registerHandler(Class<T> type, CommandHandlerMethod<T> handler);
+    <T extends BaseCommand> void registerHandler(Class<T> type, CommandHandleMethod<T> handler);
     void send(BaseCommand command);
 }
